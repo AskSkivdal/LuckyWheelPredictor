d7483b0b7dd45e1b2de5ce062d97320d26280642
slasken06 <56585404+slasken06@users.noreply.github.com>
2022-06-12 16:44:12 +0200

    Made shit look good.

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
-const version = '1.0.1';
+const version = "1.1.0";
 
 // On install - the application shell cached
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('sw-cache').then(function(cache) {
-            return cache.addAll(["./index.html","./master.css"]);
-        })
-    );
+self.addEventListener("install", function (event) {
+	event.waitUntil(
+		caches.open("sw-cache").then(function (cache) {
+			return cache.addAll(["./index.html", "./master.css"]);
+		})
+	);
 });
 
 // With network request
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener("fetch", function (event) {
+	event.respondWith(
+		caches.match(event.request).then(function (response) {
+			return response || fetch(event.request);
+		})
+	);
 });
